befc4c7374f9f420689352cc095a9a5ad9bcb47a
zyz <lanyu_1999@foxmail.com>
2018-10-02 17:13:30 +0800

    删除,新增,修改品牌列表  day03

--- pinyougou/pinyougou-manage-web/src/main/java/com/pinyougou/manage/controller/BrandController.java
+++ pinyougou/pinyougou-manage-web/src/main/java/com/pinyougou/manage/controller/BrandController.java
@@ -4,12 +4,10 @@ import com.alibaba.dubbo.config.annotation.Reference;
 import com.pinyougou.pojo.TbBrand;
 import com.pinyougou.sellergoods.service.BrandService;
 import com.pinyougou.vo.PageResult;
+import com.pinyougou.vo.Result;
 import com.sun.org.apache.regexp.internal.RE;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,12 +24,51 @@ public class BrandController {
 
     @GetMapping("/testPage")
     public List<TbBrand> testPage(Integer page, Integer rows) {
-        return (List<TbBrand>) brandService.findPage(page,rows).getRows();
+        return (List<TbBrand>) brandService.findPage(page, rows).getRows();
     }
 
     @GetMapping("/findPage")
-    public PageResult findPage (@RequestParam(value = "page",defaultValue = "1")Integer page,@RequestParam(value = "rows",defaultValue = "10")Integer rows){
+    public PageResult findPage(@RequestParam(value = "page", defaultValue = "1") Integer page, @RequestParam(value = "rows", defaultValue = "10") Integer rows) {
 
-        return brandService.findPage(page,rows);
+        return brandService.findPage(page, rows);
     }
+
+    @PostMapping("/add")
+    public Result add(@RequestBody TbBrand brand) {
+        try {
+            brandService.add(brand);
+            return Result.ok("新增成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return Result.fail("新增失败");
+    }
+
+    @GetMapping("/findOne")
+    public TbBrand findOne(Long id) {
+        return brandService.findOne(id);
+    }
+
+    @PostMapping("/update")
+    public Result update(@RequestBody TbBrand brand) {
+        try {
+            brandService.update(brand);
+            return Result.ok("修改成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return Result.fail("修改失败");
+    }
+
+    @GetMapping("/delete")
+    public Result delete(Long[] ids) {
+        try {
+            brandService.deleteByIds(ids);
+            return Result.ok("删除成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return Result.fail("删除失败");
+    }
+
 }
