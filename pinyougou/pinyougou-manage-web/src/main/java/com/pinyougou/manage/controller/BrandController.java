7b65f3c727c7e9b0c656b61e5fd0ee852212eaec
zyz <lanyu_1999@foxmail.com>
2018-10-02 14:30:01 +0800

    angularJS 分页查询平品牌数据  day2 finish

--- pinyougou/pinyougou-manage-web/src/main/java/com/pinyougou/manage/controller/BrandController.java
+++ pinyougou/pinyougou-manage-web/src/main/java/com/pinyougou/manage/controller/BrandController.java
@@ -3,9 +3,12 @@ package com.pinyougou.manage.controller;
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.pinyougou.pojo.TbBrand;
 import com.pinyougou.sellergoods.service.BrandService;
+import com.pinyougou.vo.PageResult;
+import com.sun.org.apache.regexp.internal.RE;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -24,5 +27,11 @@ public class BrandController {
     @GetMapping("/testPage")
     public List<TbBrand> testPage(Integer page, Integer rows) {
         return (List<TbBrand>) brandService.findPage(page,rows).getRows();
+    }
+
+    @GetMapping("/findPage")
+    public PageResult findPage (@RequestParam(value = "page",defaultValue = "1")Integer page,@RequestParam(value = "rows",defaultValue = "10")Integer rows){
+
+        return brandService.findPage(page,rows);
     }
 }
